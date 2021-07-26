a0fd65d85b73f7217544dd6c172265a6baba89de
wangp <822371090@qq.com>
2021-07-26 20:44:51 +0800

    edit one by localdev 01

---
+++ Mybaties/src/main/java/org/wangp/service/impl/UserService.java
@@ -0,0 +1,12 @@
+package org.wangp.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.wangp.pojo.User;
+
+/**
+ * 2021/7/26
+ *
+ * @author PingW
+ */
+public interface UserService extends IService<User> {
+}
