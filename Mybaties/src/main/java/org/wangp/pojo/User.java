25e08312691a845a1fb6def364504741ab0a76ab
wangp <822371090@qq.com>
2021-07-27 08:43:20 +0800

    edit one by localdev 04

--- Mybaties/src/main/java/org/wangp/pojo/User.java
+++ Mybaties/src/main/java/org/wangp/pojo/User.java
@@ -1,9 +1,21 @@
 package org.wangp.pojo;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
 /**
  * 2021/7/26
  *
  * @author PingW
  */
+@Data
+@TableName("user")
 public class User {
+    @TableId(type = IdType.ASSIGN_ID)
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
 }
